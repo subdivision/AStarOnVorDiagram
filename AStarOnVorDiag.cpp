b04027342416a634cddd9ba3039fc930e71c327d
subdivision <eug@post.com>
2020-11-15 01:11:13 +0200

    Adding inter-slice edges

--- AStarOnVorDiag.cpp
+++ AStarOnVorDiag.cpp
@@ -209,6 +209,7 @@ int main(int argc, char** argv)
     set<int> setCurrSliceIdx;
     while (true)
     {
+        setCurrSliceIdx.clear();
         if (!read_slice(data, currZ,
             vecVertices, vecEdges, vecWeights,
             mapLookup, setCurrSliceIdx))
